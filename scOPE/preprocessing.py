61f74ec5ad5c26b6cb8b71b47be5227b8871f4d9
ashforda <ashforda@exanode-09-19.local>
2024-05-20 15:37:46 -0700

    Wrote some preprocessing code. Edited the Jupyter notebook to use the preprocessing code.

--- scOPE/preprocessing.py
+++ scOPE/preprocessing.py
@@ -1,2 +1,23 @@
-print('sups')
+# Import modules
+import pandas as pd
 
+
+# Define functions
+def tsv_to_df(file_path, delimiter='\t', index_col=None):
+    '''
+    Function that takes in a .tsv file and stores it in a pandas dataframe
+    
+    Delimiter defaults to '\t'
+    index_col defaults to none, in the case of wanting to name the rows after the first column, use index_col=0
+    otherwise, row names default to the row count index
+    
+    Returns Pandas dataframe of tsv file contents
+    '''
+    
+    dataframe = pd.read_csv(file_path, delimiter=delimiter, index_col=index_col)
+    
+    return dataframe
+    
+
+
+    
